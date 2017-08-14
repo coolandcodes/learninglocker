626cd24c816d3c2d09bfbd1d519bbe55b6466677
Synergixe TMS <synergixe.org@gmail.com>
2017-08-14 11:35:40 -0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -4,13 +4,23 @@ return [
   'fetch' => PDO::FETCH_CLASS,
   'default' => 'mongodb',
   'connections' => [
+    'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'synergixe',
+            'username'  => '',
+            'password'  => ''
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+    ],
 		'mongodb' => [
-        'driver'   => 'mongodb',
-        'host'     => 'localhost',
-        'port'     => 27017,
-        'username' => '',
-        'password' => '',
-        'database' => 'll_staging' // Default name (removing this makes Travis fail).
+            'driver'   => 'mongodb',
+            'host'     => 'localhost',
+            'port'     => 27017,
+            'username' => '',
+            'password' => '',
+            'database' => 'll_staging' // Default name (removing this makes Travis fail).
     ],
 	],
 	'migrations' => 'migrations',
